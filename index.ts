71787ab4e527c90f755eab123b9d7e94b0de8c42
truepartner312 <truepartner312@gmail.com>
2024-02-06 02:17:46 +0900

    server-set

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import socket from '@Conf/socket'
 socket(httpsServer)
 socket(httpServer)
 
-const PORT = process.env.PORT || 80
+const HTTP_PORT = process.env.HTTP_PORT || 80
+const HTTPS_PORT = process.env.HTTPS_PORT || 443
 
-httpsServer.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+httpsServer.listen(HTTP_PORT, () => console.log(`Http Server is running on port ${HTTP_PORT}`))
+httpsServer.listen(HTTPS_PORT, () => console.log(`Http Server is running on port ${HTTPS_PORT}`))
