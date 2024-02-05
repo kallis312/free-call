f443a7325331489638ad8afd6adccc73c03fb74d
truepartner312 <truepartner312@gmail.com>
2024-02-05 23:29:59 +0900

    port-update

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import socket from '@Conf/socket'
 
 socket(httpsServer)
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 
 httpsServer.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
