85f3858cb90d89cd3be94051d5f15b20b60a85db
truepartner312 <truepartner312@gmail.com>
2024-02-08 07:06:52 +0900

    update-push

---
+++ public/assets/index-C19nJKgj.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-nv4SWg1Q.js";const r=()=>t.jsx("div",{children:"About"});export{r as default};
--- public/assets/index-CoZ4-1-u.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-nH1sHtVQ.js";const r=()=>t.jsx("div",{children:"About"});export{r as default};
