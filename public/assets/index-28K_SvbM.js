0727867b2a64aaf777f3d4f776372c52957c05f3
truepartner312 <truepartner312@gmail.com>
2024-02-16 01:39:33 +0900

    Updated: `date +'date /t'`

---
+++ public/assets/index-28K_SvbM.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-1vjspzz-.js";const r=()=>t.jsx("div",{children:"About"});export{r as default};
--- public/assets/index-E29lkGEg.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-RBq5vjLg.js";const r=()=>t.jsx("div",{children:"About"});export{r as default};
