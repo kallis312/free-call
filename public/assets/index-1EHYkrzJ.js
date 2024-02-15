8cdf338742c752c64e510eae244031ce8a8f91f5
truepartner312 <truepartner312@gmail.com>
2024-02-15 21:23:46 +0900

    update

--- public/assets/index-1EHYkrzJ.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-HdygEW_4.js";const r=()=>t.jsx("div",{children:"About"});export{r as default};
---
+++ public/assets/index-gejGoDOa.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-d715ifzC.js";const r=()=>t.jsx("div",{children:"About"});export{r as default};
